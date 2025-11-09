eb33a805f799855190ba845de25c6d7c9707a370
Mehedi Hasan <mehedihasan624358@gmail.com>
2025-11-09 14:12:55 +0600

    header footer add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { AppsProvider } from "./AppsContext";
+import { router } from "./routes";
+
+
+createRoot(document.getElementById("root")).render(
+  <AppsProvider>
+    <RouterProvider router={router} />
+  </AppsProvider>
+);
